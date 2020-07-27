19132b41d8258b020ffe1defdb15c98296e82dd6
Ft-N <ftnfata@gmail.com>
2020-07-27 16:08:45 +0700

    Fix bug parser

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ keyword = os.environ.get("KEYWORD")
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
